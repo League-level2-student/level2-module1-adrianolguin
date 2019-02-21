ef0b6fd97153473b5c4e3ffd467cb3b66e975b32
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-20 18:30:13 -0800

    GuestBook

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,6 +1,13 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +16,38 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	
+	
+	static JFrame frame = new JFrame();
+	static JPanel panel = new JPanel();
+	static JButton button1 = new JButton("Add Name");
+	static JButton button2 = new JButton("View Names");
+	
+	void createGUI() {
+	frame.setVisible(true);
+	frame.add(panel);
+	panel.add(button1);
+	button1.setEnabled(true);
+	button1.addActionListener(this);
+	panel.add(button2);
+	button2.setEnabled(true);
+	button2.addActionListener(this);
+	frame.pack();
+	
+}
+	
+	public static void main(String[] args) {
+		
+		GuestBook gb = new GuestBook();
+		
+		gb.createGUI();
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
